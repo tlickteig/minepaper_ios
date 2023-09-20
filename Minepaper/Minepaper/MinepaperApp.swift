fa596599e636f2b48feedcd9562c648a4ad8e80b
tlickteig <timmattmelnat@gmail.com>
2023-09-20 18:57:04 -0500

    Initial project commit

---
+++ Minepaper/Minepaper/MinepaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinepaperApp.swift
+//  Minepaper
+//
+//  Created by Timothy Lickteig on 9/20/23.
+//
+
+import SwiftUI
+
+@main
+struct MinepaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
