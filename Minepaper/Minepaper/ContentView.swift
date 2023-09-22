5fab14f020e1d57abc59324f396dbf2c9c761fff
tlickteig <timmattmelnat@gmail.com>
2023-09-21 19:12:51 -0500

    Got lazy loading working

--- Minepaper/Minepaper/ContentView.swift
+++ Minepaper/Minepaper/ContentView.swift
@@ -10,45 +10,59 @@ import SwiftUI
 struct ContentView: View {
     
     @State var images = [WallpaperOption]()
+    @State var images2 = [String]()
     @State var isLoading = true
     
     var body: some View {
         VStack {
             NavigationView {
-                VStack {
-                    List(images, id: \.id) { image in
-                        Text(image.imageName)
-                    }
-                    .task {
-                        DispatchQueue.global(qos: .userInitiated).async {
-                            do {
-                                defer {
-                                    isLoading = false
-                                }
-                                
-                                let imageList = try Utilities.getImageListFromServer()
-                                for image in imageList {
-                                    images.append(WallpaperOption(imageName: image))
-                                }
-                            }
-                            catch {
+                if !isLoading {
+                    /*VStack {
+                        List(images, id: \.id) { image in
+                            AsyncImage(url: URL(string: image.fullImageUrl)) { image in
+                                image.image?
+                                    .resizable()
+                                    .scaledToFill()
                                 
                             }
                         }
                     }
-                    .opacity(isLoading ? 0 : 1)
-                    
-                    VStack {
-                        ProgressView()
-                        Text("")
-                        Text("Downloading images...")
+                    .navigationTitle("Minepaper")*/
+                    ScrollView {
+                        LazyVStack {
+                            ForEach(images2, id: \.self) { image in
+                                Text(image)
+                            }
+                        }
                     }
-                    .opacity(isLoading ? 1 : 0)
-                }.navigationTitle("Minepaper")
+                }
+                
+                if isLoading {
+                    ProgressView()
+                    Text("")
+                    Text("Downloading images...")
+                }
             }
             .navigationBarTitleDisplayMode(.inline)
+            .task {
+                DispatchQueue.global(qos: .userInitiated).async {
+                    do {
+                        defer {
+                            isLoading = false
+                        }
+                        
+                        let imageList = try Utilities.getImageListFromServer()
+                        images2 = imageList
+                        for image in imageList {
+                            images.append(WallpaperOption(imageName: image))
+                        }
+                    }
+                    catch {
+                        
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
