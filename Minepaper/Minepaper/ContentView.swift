fcdfc9d6c6b3cf7c43906e6c99ae8ddafa33f49a
tlickteig <timmattmelnat@gmail.com>
2023-09-23 13:05:42 -0500

    Got lazy loading working for images

--- Minepaper/Minepaper/ContentView.swift
+++ Minepaper/Minepaper/ContentView.swift
@@ -6,12 +6,14 @@
 //
 
 import SwiftUI
+import AsyncDownSamplingImage
 
 struct ContentView: View {
     
     @State var images = [WallpaperOption]()
     @State var images2 = [String]()
     @State var isLoading = true
+    @State private var size: CGSize = .init(width: 160, height: 90)
     
     var body: some View {
         VStack {
@@ -31,7 +33,26 @@ struct ContentView: View {
                     ScrollView {
                         LazyVStack {
                             ForEach(images2, id: \.self) { image in
-                                Text(image)
+                                //Text(image)
+                                /*AsyncImage(url: URL(string: image.)) { image in
+                                 image.image?
+                                 .resizable()
+                                 .scaledToFill()
+                                 
+                                 }*/
+                                let url = "https://cdn.minepaper.net/\(image)"
+                                //LazyLoadedImage(url: url)
+                                AsyncDownSamplingImage(url: URL(string: url), downsampleSize: size
+                                ) { image in image
+                                    .resizable()
+                                    .scaledToFill()
+                                    .padding(1)
+                                    .cornerRadius(10)
+                                } fail: { error in
+                                  Text("Error loading image")
+                                }
+                                .cornerRadius(10)
+                                .frame(width: 150)
                             }
                         }
                     }
@@ -65,6 +86,35 @@ struct ContentView: View {
         }
     }
 }
+
+/*
+struct LazyLoadedImage: View {
+    
+    @State var initialImage = UIImage()
+    @State var imageUrl: String
+    
+    init (url: String) {
+        imageUrl = url
+    }
+    
+    var body: some View {
+        Image(uiImage: initialImage)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .onAppear {
+                let url = URL(string: imageUrl)
+                URLSession.shared.dataTask(with: url!) { (data, response, error) in
+                    guard let data = data else { return }
+                    guard let image = UIImage(data: data) else { return }
+
+                    RunLoop.main.perform {
+                        self.initialImage = image
+                    }
+
+                }.resume()
+            }
+    }
+}*/
 
 #Preview {
     ContentView()
