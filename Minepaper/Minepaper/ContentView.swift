ba85a5073717c7082fec0ba5d3150cdcea539d26
tlickteig <timmattmelnat@gmail.com>
2023-09-23 15:56:13 -0500

    General UI updates

--- Minepaper/Minepaper/ContentView.swift
+++ Minepaper/Minepaper/ContentView.swift
@@ -10,61 +10,74 @@ import AsyncDownSamplingImage
 
 struct ContentView: View {
     
-    @State var images = [WallpaperOption]()
-    @State var images2 = [String]()
+    @State var images = [String]()
+    @State var loadedImages = [String]()
     @State var isLoading = true
-    @State private var size: CGSize = .init(width: 160, height: 90)
+    @State private var size: CGSize = .init(width: 640, height: 360)
+    //@State private var size: CGSize = .init(width: 384, height: 216)
     
     var body: some View {
-        VStack {
-            NavigationView {
-                if !isLoading {
-                    /*VStack {
-                        List(images, id: \.id) { image in
-                            AsyncImage(url: URL(string: image.fullImageUrl)) { image in
-                                image.image?
-                                    .resizable()
-                                    .scaledToFill()
-                                
-                            }
-                        }
-                    }
-                    .navigationTitle("Minepaper")*/
-                    ScrollView {
-                        LazyVStack {
-                            ForEach(images2, id: \.self) { image in
-                                //Text(image)
-                                /*AsyncImage(url: URL(string: image.)) { image in
-                                 image.image?
-                                 .resizable()
-                                 .scaledToFill()
-                                 
-                                 }*/
-                                let url = "https://cdn.minepaper.net/\(image)"
-                                //LazyLoadedImage(url: url)
-                                AsyncDownSamplingImage(url: URL(string: url), downsampleSize: size
-                                ) { image in image
-                                    .resizable()
-                                    .scaledToFill()
-                                    .padding(1)
-                                    .cornerRadius(10)
-                                } fail: { error in
-                                  Text("Error loading image")
-                                }
-                                .cornerRadius(10)
-                                .frame(width: 150)
-                            }
-                        }
-                    }
-                }
-                
-                if isLoading {
-                    ProgressView()
-                    Text("")
-                    Text("Downloading images...")
+        /*ScrollView {
+            LazyVStack {
+                ForEach(images, id: \.self) { image in
+                    Text(image)
                 }
             }
-            .navigationBarTitleDisplayMode(.inline)
+        }*/
+        
+        VStack {
+            NavigationView {
+                Form {
+                    Section {
+                        if !isLoading {
+                            ScrollView {
+                                LazyVStack {
+                                    ForEach(images, id: \.self) { image in
+                                        let url = "https://cdn.minepaper.net/\(image)"
+                                        AsyncDownSamplingImage(url: URL(string: url), downsampleSize: size
+                                         ) { image in image
+                                         .resizable()
+                                         .scaledToFill()
+                                         .padding(1)
+                                         .cornerRadius(15)
+                                         } fail: { error in
+                                             Text("Error loading image")
+                                         }
+                                         .cornerRadius(15)
+                                    }
+                                }
+                            }
+                            
+                            /*List {
+                                ForEach(images2, id: \.self) { image in
+                                    LazyVStack {
+                                        let url = "\(Constants.remoteImagesFolder)/\(image)"
+                                        AsyncImage(url: URL(string: url)) { image in
+                                            image.image?
+                                                .resizable()
+                                                .scaledToFill()
+                                                
+                                        }
+                                    }
+                                }
+                            }*/
+                        }
+                        
+                        if isLoading {
+                            VStack {
+                                ProgressView()
+                                Text("Getting images...")
+                            }
+                            .frame(maxWidth: .infinity, alignment: .center)
+                        }
+                    }
+                    /*.gesture(DragGesture(minimumDistance: 0, coordinateSpace: .local)
+                        .onChanged({ value in
+                            print("Hello World!")
+                        }))*/
+                }
+                .navigationTitle("Minepaper")
+            }
             .task {
                 DispatchQueue.global(qos: .userInitiated).async {
                     do {
@@ -72,11 +85,18 @@ struct ContentView: View {
                             isLoading = false
                         }
                         
-                        let imageList = try Utilities.getImageListFromServer()
-                        images2 = imageList
-                        for image in imageList {
-                            images.append(WallpaperOption(imageName: image))
-                        }
+                        images = try Utilities.getImageListFromServer()
+                        loadMoreImages()
+                        /*for image in images {
+                            let url = "\(Constants.remoteImagesFolder)/\(image)"
+                            let view = AsyncDownSamplingImage(url: URL(string: url), downsampleSize: size
+                            ) { precache in precache
+                                
+                            }
+                            fail: { error in
+                                
+                            }
+                        }*/
                     }
                     catch {
                         
@@ -85,36 +105,23 @@ struct ContentView: View {
             }
         }
     }
-}
-
-/*
-struct LazyLoadedImage: View {
     
-    @State var initialImage = UIImage()
-    @State var imageUrl: String
-    
-    init (url: String) {
-        imageUrl = url
-    }
-    
-    var body: some View {
-        Image(uiImage: initialImage)
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .onAppear {
-                let url = URL(string: imageUrl)
-                URLSession.shared.dataTask(with: url!) { (data, response, error) in
-                    guard let data = data else { return }
-                    guard let image = UIImage(data: data) else { return }
-
-                    RunLoop.main.perform {
-                        self.initialImage = image
-                    }
-
-                }.resume()
+    private func loadMoreImages() {
+        if !loadedImages.isEmpty {
+            let lastImageLoaded = loadedImages.last ?? ""
+            let indexOfLastImageLoaded = images.lastIndex(of: lastImageLoaded) ?? 0
+            
+            for i in indexOfLastImageLoaded..<(indexOfLastImageLoaded + 20) {
+                loadedImages.append(images[i])
             }
+        }
+        else {
+            for i in 0..<(0 + 20) {
+                loadedImages.append(images[i])
+            }
+        }
     }
-}*/
+}
 
 #Preview {
     ContentView()
