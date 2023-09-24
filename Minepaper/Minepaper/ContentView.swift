8b8811baf46496bef62118107b7e2c55b11cd4e1
tlickteig <timmattmelnat@gmail.com>
2023-09-24 12:06:20 -0500

    Improvements to lazy loading performance and added details view

--- Minepaper/Minepaper/ContentView.swift
+++ Minepaper/Minepaper/ContentView.swift
@@ -11,6 +11,7 @@ import AsyncDownSamplingImage
 struct ContentView: View {
     
     @State var images = [String]()
+    @State var loadedImages = [String]()
     @State var isLoading = true
     @State private var size: CGSize = .init(width: 640, height: 360)
     
@@ -21,18 +22,28 @@ struct ContentView: View {
                     if !isLoading {
                         ScrollView {
                             LazyVStack {
-                                ForEach(images, id: \.self) { image in
-                                    let url = "https://cdn.minepaper.net/\(image)"
-                                    AsyncDownSamplingImage(url: URL(string: url), downsampleSize: size
-                                    ) { image in image
-                                            .resizable()
-                                            .scaledToFill()
-                                            .padding(1)
-                                            .cornerRadius(15)
-                                    } fail: { error in
-                                        Text("Error loading image")
+                                ForEach(loadedImages, id: \.self) { image in
+                                    Spacer()
+                                    let url = "\(Constants.remoteImagesFolder)/\(image)"
+                                    NavigationLink(destination: WallpaperView()) {
+                                        AsyncDownSamplingImage(url: URL(string: url), downsampleSize: size
+                                        ) { image in image
+                                                .resizable()
+                                                .scaledToFit()
+                                                .padding(1)
+                                                .cornerRadius(15)
+                                        } fail: { error in
+                                            Text("Error loading image")
+                                        }
+                                        .onAppear {
+                                            if image == loadedImages.last {
+                                                loadMoreImages()
+                                            }
+                                        }
+                                        .cornerRadius(15)
+                                        .frame(height: 180)
                                     }
-                                    .cornerRadius(15)
+                                    Spacer()
                                 }
                             }
                         }
@@ -47,6 +58,7 @@ struct ContentView: View {
                     }
                 }
                 .navigationTitle("Minepaper")
+                .padding()
             }
             .task {
                 DispatchQueue.global(qos: .userInitiated).async {
@@ -56,11 +68,31 @@ struct ContentView: View {
                         }
                         
                         images = try Utilities.getImageListFromServer()
+                        loadMoreImages()
                     }
                     catch {
                         
                     }
                 }
+            }
+        }
+    }
+    
+    private func loadMoreImages() {
+        if !loadedImages.isEmpty {
+            let lastImageLoaded = loadedImages.last ?? ""
+            let indexOfLastImageLoaded = images.lastIndex(of: lastImageLoaded) ?? 0
+            let indexToStartAt = indexOfLastImageLoaded + 1
+            
+            for i in indexToStartAt..<(indexToStartAt + 5) {
+                if i < images.count {
+                    loadedImages.append(images[i])
+                }
+            }
+        }
+        else {
+            for i in 0..<(0 + 5) {
+                loadedImages.append(images[i])
             }
         }
     }
---
+++ Minepaper/Minepaper/WallpaperView.swift
@@ -0,0 +1,18 @@
+//
+//  WallpaperView.swift
+//  Minepaper
+//
+//  Created by Timothy Lickteig on 9/24/23.
+//
+
+import SwiftUI
+
+struct WallpaperView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WallpaperView()
+}
