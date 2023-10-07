97894502e417985f9f3bfefcb422ba0730ea3386
tlickteig <timmattmelnat@gmail.com>
2023-10-07 14:54:01 -0500

    Fixed larger than necessary navigation bar

--- Minepaper/Minepaper/Views/WallpaperLink.swift
+++ Minepaper/Minepaper/Views/WallpaperLink.swift
@@ -61,6 +61,7 @@ struct WallpaperLink: View {
         }
         .navigationBarBackButtonHidden(true)
         .navigationBarItems(leading: backButton)
+        .navigationBarTitleDisplayMode(.inline)
     }
     
     func initialize() {
